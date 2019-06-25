ade402c102c5dff1fbabc799b0a40c4fff5cde37
F4NT0 <gabriel.stundner@acad.pucrs.br>
2019-06-25 18:58:46 -0300

    armazenando valor do ponteiro

---
+++ armValorPonteiro.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/*-------------------------------------------------------------------
+    TESTANDO O ARMAZENAMENTO DO VALOR DE UM PONTEIRO EM UMA VARIAVEL
+--------------------------------------------------------------------*/
+
+int main(){
+    int valor = 200; // VALOR INTEIRO DE TESTE
+    int* ponteiro = &valor; // É UM PONTEIRO PARA UM VALOR INTEIRO
+
+    char teste = 'a'; // CARACTERE DE TESTE
+    char* ponteiroTeste = &teste; // PONTEIRO PARA UM CARACTERE CHAR
+
+    printf("------------------------------------------------------\n");
+    printf("VALOR INTEIRO TESTE: %d\n",valor);
+    printf("PONTEIRO PARA O VALOR INTEIRO: %p\n",ponteiro);
+    printf("VALOR DE CARACTERE DE TESTE: %c\n",teste);
+    printf("PONTEIRO PARA O CARACTERE ARMAZENADO: %p\n",ponteiroTeste);
+    printf("------------------------------------------------------\n");
+
+}
